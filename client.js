b1e6093fe31af66006bef17ef5ab60a1b10dc29f
shortytheman <shor7ty@gmail.com>
2024-04-29 21:57:07 +0200

    Hello server/clienet

---
+++ client.js
@@ -0,0 +1,16 @@
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.on('open', () => {
+  console.log('Connected to server');
+  ws.send('Hello, server!');
+});
+
+ws.on('message', (message) => {
+  console.log(`Received from server: ${message}`);
+});
+
+ws.on('close', () => {
+  console.log('Connection closed');
+});
---
+++ server.js
@@ -0,0 +1,20 @@
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//full duplex bi-directional real-time communication via Websocket protocol instead of HTTP.
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`); //Line from client: ws.send("Hello, Server!")
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server listening on port 8080');
